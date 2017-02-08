efd8248b37cbc8b11383415d928214dbe5810641
Jiashen Wang <jiashenwang@Jiashens-MacBook-Pro.local>
2017-02-08 15:22:24 -0800

    jw - finish lab5

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -10,6 +10,9 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Javascript connected!");
+	$(".friend-name").click(function(e) {
+		$(this).text(anagrammedName($(this).text()));
+	});
 }
 
 function anagrammedName(name) {
--- routes/add.js
+++ routes/add.js
@@ -2,4 +2,10 @@ var data = require("../data.json");
 
 exports.addFriend = function(req, res) {    
 	// Your code goes here
+	data.friends.push({
+		"name": req.query.name,
+		"description": req.query.description,
+		"imageURL": "http://lorempixel.com/500/500/people/"
+	});
+	res.render('index', data);
  }
